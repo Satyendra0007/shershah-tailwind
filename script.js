4bae2a6864d32523dcb946fa236a65f55041b566
satyendra <skchandrawansi03@gmail.com>
2023-03-11 08:16:26 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('hamburger').onclick = () => {
+    let menu = document.getElementById('menu')
+    if (menu.style.display == "block"){
+        menu.style.display = "none"
+        document.body.classList.remove("stop-scrolling");
+        
+    }
+    else{
+        menu.style.display = "block"
+        document.body.classList.add("stop-scrolling");
+    }
+
+
+}
+let imageSource = ["images/S1.jpg","images/S2.jpg","images/S3.jpg","images/S4.jpg"]
+let i = 0;
+setInterval(()=>{
+    if(i === 4)
+        i = 0;
+    document.getElementById('slide').src = imageSource[i] 
+    i++;
+
+},2000)
